bb1d47c616a4771e3ef4a512ea81fd225f71336b
sunny749 <sunnygante12345@gmail.com>
2024-03-01 14:13:27 +0530

    episode 2 finished

--- App.js
+++ App.js
@@ -1,3 +1,5 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 // if we want to create a nested divs lets see how we can create that structure
 
 // const Parent=React.createElement('div',{id:'parent'},
@@ -5,7 +7,7 @@
 //             React.createElement('h1',{},'iam h1 subChaild')
 //             )
 // )
-// if we want to create more than one chaild in the same lavel the  we have to pass
+// if we want to create more than one chaild in the same level the  we have to pass
 // array of child elements lets see example
 
 const Parent=React.createElement('div',{id:'parent'},
@@ -13,7 +15,16 @@ const Parent=React.createElement('div',{id:'parent'},
             [React.createElement('h1',{},'iam h1 subChaild'),React.createElement('h2',{},'iam h2 subChaild in same level')]
             )
 )
-
+// if we want to create lots of childrens in the same level and nested level
+// then the code locks so difficult to understand thats why the react is 
+// using jsx syntax that is same as html and some javascript 
+// and if we use render method it acually replace the content if any content
+// is present in the root html element and react is a library we can use react
+// in any frame work and we can use react to build larger scale applications
+// cdn(content delivery network) is working with internet it connect the user 
+// with the nearest server and inject the react or any techinology we want 
+// and cross-origin is using because we inject different code from differet 
+// origins the cross origin gives securety the order of screept tags are mandatory
 
 
 // const headding=React.createElement('h1',{id:'headding'},'Hellow World From React')
--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 Namasthe react course 
+
+# parcel
+parcel gives some cool features that are
+1 dev build
+2 local server
+3 hmr -hot module replacement
+4 it uses File watching algorithem that written in c++
+5 parcel gives faster builds using caching(.parcel-cache)
+6 it is also take care of expensive thing that is imageOptimization
+7 when ever we build on dev mode or prodction mode it minify the files
+8 and also bundle the code
+9 compress the code
+10 consistent hashing 
+11 code splitting
+12 differential bundling -older browsers support
+13 Diagnostic and error handling
+14 with parcel we can host our app in https mode also
+15 Tree shaking -it automatically remove the code that user did not use
